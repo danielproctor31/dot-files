fc890f840578cf81e647ed816ef9284d70e7f9a7
Daniel Proctor <daniel.proctor@awaze.com>
2023-05-03 18:00:14 +0100

    add mac specific zsh install

--- zsh.sh
+++ zsh.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+is_mac() {
+    if [ "$(uname)" = "Darwin" ]; then
+        return 1;
+    else
+        return 0;
+    fi
+}
+
+# install build tools
+xcode-select --install
+
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -11,13 +22,20 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# one-time setup for font installation
-brew tap homebrew/linux-fonts
-ln -s /home/linuxbrew/.linuxbrew/share/fonts ~/.local/share/fonts
-fc-cache -fv
+# setup cask fonts
+if [ is_mac ]; then # macos
+    brew tap homebrew/cask-fonts
+else # linux
+    brew tap homebrew/linux-fonts
+    # one-time setup for font installation
+    ln -s /home/linuxbrew/.linuxbrew/share/fonts ~/.local/share/fonts
+    fc-cache -fv
+fi
 
 # install a font
 brew install font-caskaydia-cove-nerd-font
 
 # Install starship
 brew install starship
+
+echo "finished"
