f85fa518b225e540424d04278860081490790bab
Daniel Proctor <daniel.proctor@awaze.com>
2023-05-03 18:08:42 +0100

    update scripts

--- nvim.sh
+++ nvim.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
 
-echo "finished"
+echo "finished";
--- zsh.sh
+++ zsh.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 is_mac() {
     if [ "$(uname)" = "Darwin" ]; then
@@ -9,33 +9,35 @@ is_mac() {
 }
 
 # install build tools
-xcode-select --install
+xcode-select --install;
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 
 # oh-my-zsh plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
 
 # Install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! which brew; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+fi
 
 # setup cask fonts
 if [ is_mac ]; then # macos
-    brew tap homebrew/cask-fonts
+    brew tap homebrew/cask-fonts;
 else # linux
-    brew tap homebrew/linux-fonts
+    brew tap homebrew/linux-fonts;
     # one-time setup for font installation
-    ln -s /home/linuxbrew/.linuxbrew/share/fonts ~/.local/share/fonts
-    fc-cache -fv
+    ln -s /home/linuxbrew/.linuxbrew/share/fonts ~/.local/share/fonts;
+    fc-cache -fv;
 fi
 
 # install a font
-brew install font-caskaydia-cove-nerd-font
+brew install font-caskaydia-cove-nerd-font;
 
 # Install starship
-brew install starship
+brew install starship;
 
-echo "finished"
+echo "finished";
