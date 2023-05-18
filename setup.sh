f0db3fa95a1ba3beb4114d0dade267aba964408f
Daniel <10681172+danielproctor31@users.noreply.github.com>
2023-05-18 21:34:53 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,11 +8,6 @@ is_mac() {
     fi
 }
 
-# install build tools
-if is_mac; then
-    xcode-select --install;
-fi
-
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 
@@ -20,29 +15,19 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
 
-# Install brew
-if ! which brew; then
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
-fi
-
 # setup cask fonts
 if is_mac; then # macos
-    brew tap homebrew/cask-fonts;
-else # linux
-    brew tap homebrew/linux-fonts;
-    # one-time setup for font installation
-    ln -s /home/linuxbrew/.linuxbrew/share/fonts ~/.local/share/fonts;
-    # refresh font-cache
-    if which fc-cache; then
-        fc-cache -fv;
+    # install build tools
+    xcode-select --install;
+    # Install brew
+    if ! which brew; then
+        bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
     fi
+    # install packages
+    brew tap homebrew/cask-fonts;  
+    brew install \
+        font-caskaydia-cove-nerd-font \
+        starship
 fi
-
-# install packages
-brew install \
-    font-caskaydia-cove-nerd-font \
-    starship \
-    nvm \
-    dotnet@6
 
 echo "finished";
